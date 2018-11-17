e6b8e84122e46d343c6f2ac4a4e6256d183d7ea6
Roman Kerimov <roma.kerimov@icloud.com>
2018-11-17 18:09:28 +0300

    Add macOS target

---
+++ LinguisticKit/LinguisticKit.h
@@ -0,0 +1,18 @@
+//
+//  LinguisticKit.h
+//  LinguisticKit
+//
+//  Created by Roman Kerimov on 2018-11-17.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for LinguisticKit.
+FOUNDATION_EXPORT double LinguisticKitVersionNumber;
+
+//! Project version string for LinguisticKit.
+FOUNDATION_EXPORT const unsigned char LinguisticKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LinguisticKit/PublicHeader.h>
+
+
