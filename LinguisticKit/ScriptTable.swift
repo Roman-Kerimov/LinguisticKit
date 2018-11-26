7fcc96d86cfe73c856d92c178db694e88657aaf9
Roman Kerimov <roma.kerimov@icloud.com>
2018-11-26 18:33:03 +0300

    Update script translator for uppercased text

--- LinguisticKit/ScriptTable.swift
+++ LinguisticKit/ScriptTable.swift
@@ -51,12 +51,41 @@ public struct ScriptTable {
 
 public extension String {
     
+    private enum Case {
+        case lowercased, uppercased, capitalized, uppercasedOrCapitalized, uncased
+    }
+    
+    private var `case`: Case {
+        guard let firstCharacter = first else {
+            return .uncased
+        }
+        
+        if firstCharacter.isCased == false {
+            return .uncased
+        }
+        else if firstCharacter.isUppercase && count == 1 {
+            return .uppercasedOrCapitalized
+        }
+        else if filter({$0.isCased}).map({$0.isLowercase}).contains(false) == false {
+            return .lowercased
+        }
+        else if map({$0.isLowercase}).contains(true) {
+            return .capitalized
+        }
+        else if filter({$0.isCased}).map({$0.isUppercase}).contains(false) == false {
+            return .uppercased
+        }
+        else {
+            return .uncased
+        }
+    }
+    
     public func translating(from sourceScript: Script, to targetScript: Script, withTable scriptTable: ScriptTable) -> String {
         
         var scriptTable = scriptTable
         
         var index = 0
-        var targetString: String = .init()
+        var elements: [(source: String, target: String)] = .init()
         
         while index < self.count {
             var sourceElementLength = scriptTable.maxElementLength(forScript: sourceScript)
@@ -65,12 +94,12 @@ public extension String {
                 
                 let sourceElement: String = .init(self.dropFirst(index).prefix(sourceElementLength))
                 
-                if let targetElement = scriptTable.element(of: targetScript, from: sourceElement, of: sourceScript) {
-                    targetString.append(targetElement)
+                if let targetElement = scriptTable.element(of: targetScript, from: sourceElement.lowercased(), of: sourceScript) {
+                    elements.append((source: sourceElement, target: targetElement))
                     break
                 }
                 else if sourceElementLength == 1 {
-                    targetString.append(sourceElement)
+                    elements.append((source: sourceElement, target: sourceElement))
                     break
                 }
                 
@@ -80,7 +109,72 @@ public extension String {
             index += sourceElementLength
         }
         
-        return targetString
+        var elementCases: [Case] = elements.map {$0.source.case}
+        
+        for (index, elementCase) in elementCases.enumerated() {
+            guard elementCase == .uppercasedOrCapitalized else {
+                continue
+            }
+            
+            let enumeratedElementCasesBeforeAndAfter = Array.init(elementCases.enumerated()).split {$0.offset == index}
+            
+            guard let elementBefore = enumeratedElementCasesBeforeAndAfter.first?.last?.element else {
+                continue
+            }
+            
+            guard let elementAfter = enumeratedElementCasesBeforeAndAfter.last?.first?.element else {
+                continue
+            }
+            
+            let newElementCase: Case
+            
+            switch (elementBefore, elementAfter) {
+                
+            case (_, .uncased):
+                continue
+                
+            case (_, .lowercased):
+                newElementCase = .capitalized
+                
+            default:
+                newElementCase = .uppercased
+            }
+            
+            elementCases[index] = newElementCase
+        }
+
+        for (index, _) in elementCases.enumerated().filter({$0.element == .uppercasedOrCapitalized}) {
+
+            let casedElelementsBeforeAndAfter = elements.enumerated().filter({[.uppercasedOrCapitalized, .uncased].contains($0.element.source.case) == false}) .split {$0.offset == index}
+
+            let contextCases = [casedElelementsBeforeAndAfter.first?.last?.element.source.case, casedElelementsBeforeAndAfter.last?.first?.element.source.case]
+
+            if contextCases.compactMap({$0}).contains(.uppercased) {
+                elementCases[index] = .uppercased
+            }
+            else {
+                elementCases[index] = .capitalized
+            }
+        }
+        
+        for (index, elementCase) in elementCases.enumerated() {
+            switch elementCase {
+                
+            case .lowercased:
+                elements[index].target = elements[index].target.lowercased()
+                
+            case .uppercased:
+                elements[index].target = elements[index].target.uppercased()
+                
+            case .capitalized:
+                elements[index].target = elements[index].target.capitalized
+                
+            case .uppercasedOrCapitalized, .uncased:
+                continue
+            }
+        }
+        
+        return elements.map {$0.target} .joined()
     }
 }
 
@@ -145,9 +239,6 @@ public extension ScriptTable {
         
         elements += consonantLetters.map {[.Latn: $0[.Latn]! + "e", .Cyrl: $0[.Cyrl]! + "е"]}
         elements += consonantLetters.map {[.Latn: $0[.Latn]! + "ye", .Cyrl: $0[.Cyrl]! + "э"]}
-        
-        
-        elements += elements.map {[.Latn: $0[.Latn]!.capitalized, .Cyrl: $0[.Cyrl]!.capitalized]}
         
         return elements
     }
