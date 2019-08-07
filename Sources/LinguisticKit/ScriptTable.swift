20ba7d71b3958b8daabcdf001f3a2b727a766ba8
Roman Kerimov <roma.kerimov@icloud.com>
2019-08-07 12:05:54 +0300

    Fix typo

--- Sources/LinguisticKit/ScriptTable.swift
+++ Sources/LinguisticKit/ScriptTable.swift
@@ -14,8 +14,8 @@ public class ScriptTable {
     private let table: RAWScriptTable
     
     private lazy var scriptSet = table.map {$0.keys} .reduce(Set.init(Script.allCases)) {$0.intersection($1)}
-    lazy var scriptLetterSets: [Script: Set<Character>] = idexedScriptTables.mapValues {.init($0.keys.joined())}
-    private lazy var idexedScriptTables: [Script: IndexedScriptTable] = Dictionary.init(
+    lazy var scriptLetterSets: [Script: Set<Character>] = indexedScriptTables.mapValues {.init($0.keys.joined())}
+    private lazy var indexedScriptTables: [Script: IndexedScriptTable] = Dictionary.init(
         uniqueKeysWithValues: scriptSet.map { (script) -> (Script, IndexedScriptTable) in
             return (script, IndexedScriptTable.init(uniqueKeysWithValues: table.map { ($0[script]!, $0) } ))
         }
@@ -28,21 +28,21 @@ public class ScriptTable {
     
     internal func maxElementLength(forScript script: Script) -> Int {
         
-        return idexedScriptTables[script]?.keys.map {$0.count} .max() ?? 0
+        return indexedScriptTables[script]?.keys.map {$0.count} .max() ?? 0
     }
     
     internal func element(of targetScript: Script, from sourceElement: String, of sourceScript: Script) -> String? {
         
         
-        guard idexedScriptTables.keys.contains(sourceScript) else {
+        guard indexedScriptTables.keys.contains(sourceScript) else {
             fatalError("The script table donʼt support \(String.init(describing: sourceScript)) source script!")
         }
         
-        guard idexedScriptTables.keys.contains(targetScript) else {
+        guard indexedScriptTables.keys.contains(targetScript) else {
             fatalError("The script table donʼt support \(String.init(describing: targetScript)) target script!")
         }
         
-        guard let element = idexedScriptTables[sourceScript]?[sourceElement]?[targetScript] else {
+        guard let element = indexedScriptTables[sourceScript]?[sourceElement]?[targetScript] else {
             return nil
         }
         
