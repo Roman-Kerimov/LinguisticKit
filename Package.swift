86234593fdf49b4be46dca35a14678fe3cf5529d
Roman Kerimov <roma.kerimov@icloud.com>
2020-03-26 00:21:30 +0300

    Set dynamic library type

--- Package.swift
+++ Package.swift
@@ -9,6 +9,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "LinguisticKit",
+            type: .dynamic,
             targets: ["LinguisticKit"]),
     ],
     dependencies: [
