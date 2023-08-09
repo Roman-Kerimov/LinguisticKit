42b5aa118e7fe1c75235382da40f36e1f16205b2
Roman Kerimov <roma.kerimov@icloud.com>
2023-08-08 21:16:37 -0300

    Refactor code style in Package.swift

--- Package.swift
+++ Package.swift
@@ -1,33 +1,33 @@
-// swift-tools-version:5.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
     name: "LinguisticKit",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "LinguisticKit",
             type: .dynamic,
-            targets: ["LinguisticKit"]),
+            targets: [
+                "LinguisticKit",
+            ]
+        ),
         .library(
             name: "LinguisticKit.static",
             type: .static,
-            targets: ["LinguisticKit"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+            targets: [
+                "LinguisticKit",
+            ]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "LinguisticKit",
-            dependencies: []),
+            name: "LinguisticKit"
+        ),
         .testTarget(
             name: "LinguisticKitTests",
-            dependencies: ["LinguisticKit"]),
+            dependencies: [
+                "LinguisticKit",
+            ]
+        ),
     ]
 )
