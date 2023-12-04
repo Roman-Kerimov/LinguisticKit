6b52612e526d9540695797da2c2402b962dab264
Roman Kerimov <roma.kerimov@icloud.com>
2023-12-04 19:22:29 -0300

    Add testSubstantiveMasculineE()

--- Tests/LinguisticKitTests/WordTests.swift
+++ Tests/LinguisticKitTests/WordTests.swift
@@ -456,4 +456,58 @@ final class WordTests: XCTestCase {
             )
         )
     }
+    
+    func testSubstantiveMasculineE() {
+        // MARK: 1e
+        XCTAssert(
+            word: "zub",
+            forms: SubstantiveForms(
+                nominative: ("zub", "zúbyi"),
+                genitive: ("zúba", "zubóv"),
+                dative: ("zúbu", "zubám"),
+                accusative: ("zub", "zúbyi"),
+                instrumental: ("zúbom", "zubámi"),
+                prepositional: ("zúbe", "zubákh")
+            )
+        )
+        
+        // MARK: 2e
+        XCTAssert(
+            word: "gólubj",
+            forms: SubstantiveForms(
+                nominative: ("gólubj", "gólubi"),
+                genitive: ("gólubya", "golubéj"),
+                dative: ("gólubyu", "golubyám"),
+                accusative: ("gólubya", "golubéj"),
+                instrumental: ("gólubem", "golubyámi"),
+                prepositional: ("gólube", "golubyákh")
+            )
+        )
+        
+        // MARK: 3e
+        XCTAssert(
+            word: "volk",
+            forms: SubstantiveForms(
+                nominative: ("volk", "vólki"),
+                genitive: ("vólka", "volkóv"),
+                dative: ("vólku", "volkám"),
+                accusative: ("vólka", "volkóv"),
+                instrumental: ("vólkom", "volkámi"),
+                prepositional: ("vólke", "volkákh")
+            )
+        )
+        
+        // MARK: 4e
+        XCTAssert(
+            word: "óbruch",
+            forms: SubstantiveForms(
+                nominative: ("óbruch", "óbruchi"),
+                genitive: ("óbrucha", "obruchéj"),
+                dative: ("óbruchu", "obruchám"),
+                accusative: ("óbruch", "óbruchi"),
+                instrumental: ("óbruchem", "obruchámi"),
+                prepositional: ("óbruche", "obruchákh")
+            )
+        )
+    }
 }
