8fed644a218ded11be28058866e0d75d76d7efc8
Roman Kerimov <roma.kerimov@icloud.com>
2023-12-18 01:25:28 -0300

    Add initial forms

---
+++ Tests/LinguisticKitTests/WordTests/Substantive/Masculine/testSubstantiveMasculinePluraleTantum.swift
@@ -0,0 +1,318 @@
+//
+//  testSubstantiveMasculinePluraleTantum.swift
+//
+//
+//  Created by Roman Kerimov on 17/12/23.
+//
+
+import Foundation
+
+extension WordTests {
+    func testSubstantiveMasculinePluraleTantum() {
+        // MARK: pl <m 1a>
+        
+        XCTAssert(
+            word: "annályi",
+            forms: SubstantiveForms(
+                nominative: ("", ""),
+                genitive: ("", ""),
+                dative: ("", ""),
+                accusative: ("", ""),
+                instrumental: ("", ""),
+                prepositional: ("", "")
+            )
+        )
+        
+        // MARK: pl <m 1b>
+        
+        XCTAssert(
+            word: "vesyí",
+            forms: SubstantiveForms(
+                nominative: ("", ""),
+                genitive: ("", ""),
+                dative: ("", ""),
+                accusative: ("", ""),
+                instrumental: ("", ""),
+                prepositional: ("", "")
+            )
+        )
+        
+        // MARK: pl <m 2a>
+        
+        XCTAssert(
+            word: "débri",
+            forms: SubstantiveForms(
+                nominative: ("", ""),
+                genitive: ("", ""),
+                dative: ("", ""),
+                accusative: ("", ""),
+                instrumental: ("", ""),
+                prepositional: ("", "")
+            )
+        )
+        
+        // MARK: pl <m 2b>
+        
+        XCTAssert(
+            word: "kochkarí",
+            forms: SubstantiveForms(
+                nominative: ("", ""),
+                genitive: ("", ""),
+                dative: ("", ""),
+                accusative: ("", ""),
+                instrumental: ("", ""),
+                prepositional: ("", "")
+            )
+        )
+        
+        // MARK: pl <m 2e>
+        
+        XCTAssert(
+            word: "sáni",
+            forms: SubstantiveForms(
+                nominative: ("", ""),
+                genitive: ("", ""),
+                dative: ("", ""),
+                accusative: ("", ""),
+                instrumental: ("", ""),
+                prepositional: ("", "")
+            )
+        )
+        
+        // MARK: pl <m 3a>
+        
+        XCTAssert(
+            word: "trúsiki",
+            forms: SubstantiveForms(
+                nominative: ("", ""),
+                genitive: ("", ""),
+                dative: ("", ""),
+                accusative: ("", ""),
+                instrumental: ("", ""),
+                prepositional: ("", "")
+            )
+        )
+        
+        // MARK: pl <m 3b>
+        
+        XCTAssert(
+            word: "dukhí",
+            forms: SubstantiveForms(
+                nominative: ("", ""),
+                genitive: ("", ""),
+                dative: ("", ""),
+                accusative: ("", ""),
+                instrumental: ("", ""),
+                prepositional: ("", "")
+            )
+        )
+        
+        // MARK: pl <m 4a>
+        
+        XCTAssert(
+            word: "passatízhi",
+            forms: SubstantiveForms(
+                nominative: ("", ""),
+                genitive: ("", ""),
+                dative: ("", ""),
+                accusative: ("", ""),
+                instrumental: ("", ""),
+                prepositional: ("", "")
+            )
+        )
+        
+        // MARK: pl <m 4b>
+        
+        XCTAssert(
+            word: "sjhi",
+            forms: SubstantiveForms(
+                nominative: ("", ""),
+                genitive: ("", ""),
+                dative: ("", ""),
+                accusative: ("", ""),
+                instrumental: ("", ""),
+                prepositional: ("", "")
+            )
+        )
+        
+        // MARK: pl <m 4e>
+        
+        XCTAssert(
+            word: "drózhzhi",
+            forms: SubstantiveForms(
+                nominative: ("", ""),
+                genitive: ("", ""),
+                dative: ("", ""),
+                accusative: ("", ""),
+                instrumental: ("", ""),
+                prepositional: ("", "")
+            )
+        )
+        
+        // MARK: pl <m 6a>
+        
+        XCTAssert(
+            word: "pomói",
+            forms: SubstantiveForms(
+                nominative: ("", ""),
+                genitive: ("", ""),
+                dative: ("", ""),
+                accusative: ("", ""),
+                instrumental: ("", ""),
+                prepositional: ("", "")
+            )
+        )
+        
+        // MARK: pl <m 7a>
+        
+        XCTAssert(
+            word: "preliminárii",
+            forms: SubstantiveForms(
+                nominative: ("", ""),
+                genitive: ("", ""),
+                dative: ("", ""),
+                accusative: ("", ""),
+                instrumental: ("", ""),
+                prepositional: ("", "")
+            )
+        )
+        
+        // MARK: pl <m 2*a>
+        
+        XCTAssert(
+            word: "plávni",
+            forms: SubstantiveForms(
+                nominative: ("", ""),
+                genitive: ("", ""),
+                dative: ("", ""),
+                accusative: ("", ""),
+                instrumental: ("", ""),
+                prepositional: ("", "")
+            )
+        )
+        
+        XCTAssert(
+            word: "dróvni",
+            forms: SubstantiveForms(
+                nominative: ("", ""),
+                genitive: ("", ""),
+                dative: ("", ""),
+                accusative: ("", ""),
+                instrumental: ("", ""),
+                prepositional: ("", "")
+            )
+        )
+        
+        // MARK: pl <m 2*e>
+        
+        XCTAssert(
+            word: "dróvni",
+            forms: SubstantiveForms(
+                nominative: ("", ""),
+                genitive: ("", ""),
+                dative: ("", ""),
+                accusative: ("", ""),
+                instrumental: ("", ""),
+                prepositional: ("", "")
+            )
+        )
+        
+        // MARK: pl <m 3*a>
+        
+        XCTAssert(
+            word: "ostánki",
+            forms: SubstantiveForms(
+                nominative: ("", ""),
+                genitive: ("", ""),
+                dative: ("", ""),
+                accusative: ("", ""),
+                instrumental: ("", ""),
+                prepositional: ("", "")
+            )
+        )
+        
+        // MARK: pl <m 3*b>
+        
+        XCTAssert(
+            word: "ochkí",
+            forms: SubstantiveForms(
+                nominative: ("", ""),
+                genitive: ("", ""),
+                dative: ("", ""),
+                accusative: ("", ""),
+                instrumental: ("", ""),
+                prepositional: ("", "")
+            )
+        )
+        
+        // MARK: pl <m 5*a>
+        
+        XCTAssert(
+            word: "ploskozúbcyi",
+            forms: SubstantiveForms(
+                nominative: ("", ""),
+                genitive: ("", ""),
+                dative: ("", ""),
+                accusative: ("", ""),
+                instrumental: ("", ""),
+                prepositional: ("", "")
+            )
+        )
+        
+        // MARK: pl <m 5*b>
+        
+        XCTAssert(
+            word: "sjhipcyí",
+            forms: SubstantiveForms(
+                nominative: ("", ""),
+                genitive: ("", ""),
+                dative: ("", ""),
+                accusative: ("", ""),
+                instrumental: ("", ""),
+                prepositional: ("", "")
+            )
+        )
+        
+        // MARK: pl <m 1b(1)>
+        
+        XCTAssert(
+            word: "lesá",
+            forms: SubstantiveForms(
+                nominative: ("", ""),
+                genitive: ("", ""),
+                dative: ("", ""),
+                accusative: ("", ""),
+                instrumental: ("", ""),
+                prepositional: ("", "")
+            )
+        )
+        
+        // MARK: pl <m 2b(1)>
+        
+        XCTAssert(
+            word: "zelenyá",
+            forms: SubstantiveForms(
+                nominative: ("", ""),
+                genitive: ("", ""),
+                dative: ("", ""),
+                accusative: ("", ""),
+                instrumental: ("", ""),
+                prepositional: ("", "")
+            )
+        )
+        
+        // MARK: pl <m 3b(1)>
+        
+        XCTAssert(
+            word: "toroká",
+            forms: SubstantiveForms(
+                nominative: ("", ""),
+                genitive: ("", ""),
+                dative: ("", ""),
+                accusative: ("", ""),
+                instrumental: ("", ""),
+                prepositional: ("", "")
+            )
+        )
+    }
+}
