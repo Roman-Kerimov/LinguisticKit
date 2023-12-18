cf264969c3a89d7dec5ffab2435d487701417af4
Roman Kerimov <roma.kerimov@icloud.com>
2023-12-18 02:02:51 -0300

    Add initial forms

---
+++ Tests/LinguisticKitTests/WordTests/Substantive/Masculine/testSubstantiveMasculinePluraleTantumFrom.swift
@@ -0,0 +1,84 @@
+//
+//  testSubstantiveMasculinePluraleTantumFrom.swift
+//
+//
+//  Created by Roman Kerimov on 18/12/23.
+//
+
+import Foundation
+
+extension WordTests {
+    func testSubstantiveMasculinePluraleTantumFrom() {
+        XCTAssert(
+            word: "sapogí",
+            forms: SubstantiveForms(
+                nominative: ("", ""),
+                genitive: ("", ""),
+                dative: ("", ""),
+                accusative: ("", ""),
+                instrumental: ("", ""),
+                prepositional: ("", "")
+            )
+        )
+        
+        XCTAssert(
+            word: "glazá",
+            forms: SubstantiveForms(
+                nominative: ("", ""),
+                genitive: ("", ""),
+                dative: ("", ""),
+                accusative: ("", ""),
+                instrumental: ("", ""),
+                prepositional: ("", "")
+            )
+        )
+        
+        XCTAssert(
+            word: "chuvyáki",
+            forms: SubstantiveForms(
+                nominative: ("", ""),
+                genitive: ("", ""),
+                dative: ("", ""),
+                accusative: ("", ""),
+                instrumental: ("", ""),
+                prepositional: ("", "")
+            )
+        )
+        
+        XCTAssert(
+            word: "pogónyi",
+            forms: SubstantiveForms(
+                nominative: ("", ""),
+                genitive: ("", ""),
+                dative: ("", ""),
+                accusative: ("", ""),
+                instrumental: ("", ""),
+                prepositional: ("", "")
+            )
+        )
+        
+        XCTAssert(
+            word: "konjkí",
+            forms: SubstantiveForms(
+                nominative: ("", ""),
+                genitive: ("", ""),
+                dative: ("", ""),
+                accusative: ("", ""),
+                instrumental: ("", ""),
+                prepositional: ("", "")
+            )
+        )
+        
+        XCTAssert(
+            word: "válenki",
+            forms: SubstantiveForms(
+                nominative: ("", ""),
+                genitive: ("", ""),
+                dative: ("", ""),
+                accusative: ("", ""),
+                instrumental: ("", ""),
+                prepositional: ("", "")
+            )
+        )
+    }
+}
